9f31dfbe99cfcaddb0840373e97d765842d33170
Ashutosh Pachouri <ashutosh.pachouri_cs19@gla.ac.in>
2020-08-30 15:48:53 +0530

    Create Non-parameterized Constructor in Student.java .

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -18,5 +18,13 @@ public class Student {
         this.numberOfBooksIssuedByTheStudent = numberOfBooksIssuedByTheStudent;
         this.namesOfTheBooksIssuedByTheStudent = namesOfTheBooksIssuedByTheStudent;
     }
-
+    public Student() {
+        this.nameOfTheStudent = "";
+        this.universityRollNumberOfTheStudent = 0L;
+        this.numberOfBooksIssuedByTheStudent = 0;
+        this.namesOfTheBooksIssuedByTheStudent = new Book[MAXIMUM_BOOKS_STUDENT_CAN_ISSUE];
+        for (int initializingBook = 0; initializingBook < MAXIMUM_BOOKS_STUDENT_CAN_ISSUE; initializingBook++) {
+            this.namesOfTheBooksIssuedByTheStudent[initializingBook] = new Book();
+        }
+    }
 }
