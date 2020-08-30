822945eb5c835e1ffe9b11115735a1c0d911cdfb
Ashutosh Pachouri <ashutosh.pachouri_cs19@gla.ac.in>
2020-08-30 16:11:53 +0530

    showIssuedBooksByStudent method will show all the books which is issued by the Student.

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -107,4 +107,16 @@ public class Student {
         return returnSuccessful;
     }
 
+    public void showIssuedBooksByStudent() {
+        if (getNumberOfBooksIssuedByTheStudent() == 0) {
+            System.out.println("Sorry, you haven't issued any book yet.");
+        } else {
+            System.out.println("These are the books issued by you: ");
+            for (int issuedBookIndex = 0; issuedBookIndex < MAXIMUM_BOOKS_STUDENT_CAN_ISSUE; issuedBookIndex++) {
+                if (namesOfTheBooksIssuedByTheStudent[issuedBookIndex].getNameOfTheBook() != null) {
+                    System.out.print(namesOfTheBooksIssuedByTheStudent[issuedBookIndex].getNameOfTheBook() + (issuedBookIndex < getNumberOfBooksIssuedByTheStudent() - 1 ? ", " : ".\n"));
+                }
+            }
+        }
+    }
 }
