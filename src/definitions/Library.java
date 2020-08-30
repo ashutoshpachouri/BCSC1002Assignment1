cafd31ae171ae26b8eb80f0a2b4406f4057061ab
Ashutosh Pachouri <ashutosh.pachouri_cs19@gla.ac.in>
2020-08-30 15:16:37 +0530

    Add getter and setter methods in Library.java .

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -23,4 +23,13 @@ public class Library {
             this.booksThatAreCurrentlyAvailable[libraryIndex] = new Book();
         }
     }
+
+    public Book[] getBooksThatAreCurrentlyAvailable() {
+        return booksThatAreCurrentlyAvailable;
+    }
+
+    public void setBooksThatAreCurrentlyAvailable(Book[] booksThatAreCurrentlyAvailable) {
+        this.booksThatAreCurrentlyAvailable = booksThatAreCurrentlyAvailable;
+    }
+
 }
