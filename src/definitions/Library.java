45b3996cfa2493fe86f32264a68118dc91741dd8
Ashutosh Pachouri <ashutosh.pachouri_cs19@gla.ac.in>
2020-08-30 15:06:53 +0530

    Create fields in Library.java .

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -7,49 +7,8 @@
 package definitions;
 
 import java.util.Arrays;
-import java.util.Objects;
-
 public class Library {
-    private String[] Book;
+    private static final int MAXIMUM_BOOKS_IN_LIBRARY = 5;
+    private Book[] booksThatAreCurrentlyAvailable;
 
-    public Library(String[] Book){
-        this.Book = new String[5];
-    }
-    public Library() {
-        this.Book = new String[5];
-        this.Book[0] = "want to issue a new book";
-        this.Book[1] = "Return a previously issue book";
-        this.Book[2] = "Show me all my issue books.";
-        this.Book[3] = "exit";
-        //this.Book[4] = "issue the book";
-    }
-    public String[] getBook(){
-        return Book.clone();
-    }
-
-    public void setBook(String[] book) {
-        this.Book = book;
-    }
-    @Override
-    public String toString() {
-        return Arrays.toString(Book);
-    }
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Library library = (Library)o;
-        return Arrays.equals(getBook(),library.getBook());
-    }
-    @Override
-    public int hashCode() {
-        return Arrays.hashCode(getBook());
-    }
-    public void listInventory(){
-        for (String Book : this.Book){
-            if (Book != null){
-                System.out.println(Book);
-            }
-        }
-    }
 }
